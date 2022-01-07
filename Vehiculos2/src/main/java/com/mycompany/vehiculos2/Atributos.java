e8a2d4c77dc27db3f1c0b2cb6d0478c2f5a3dd36
lenovo <lenovo@LAPTOP-5HNANOAR>
2022-01-06 19:06:01 -0500

    Expo_patriks

--- Vehiculos2/src/main/java/com/mycompany/vehiculos2/Atributos.java
+++ Vehiculos2/src/main/java/com/mycompany/vehiculos2/Atributos.java
@@ -5,17 +5,98 @@
  */
 package com.mycompany.vehiculos2;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author lenovo
  */
 public class Atributos {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+         private String propietario;
+        private double valorComercial;
+        private  String color;
+         private  String placa;
+          private  String matriculado;
+           private  int aniofabricacion;
+
+    public String getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(String propietario) {
+        this.propietario = propietario;
+    }
+
+    public double getValorComercial() {
+        return valorComercial;
+    }
+
+    public void setValorComercial(double valorComercial) {
+        this.valorComercial = valorComercial;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMatriculado() {
+        return matriculado;
+    }
+
+    public void setMatriculado(String matriculado) {
+        this.matriculado = matriculado;
+    }
+
+    public int getAniofabricacion() {
+        return aniofabricacion;
+    }
+
+    public void setAniofabricacion(int aniofabricacion) {
+        this.aniofabricacion = aniofabricacion;
+    }
+           
+          
+           public Atributos (String propietario){
+               this.propietario = propietario;
+                 JOptionPane.showInternalMessageDialog(null,"BIENVENIDOS,Los ddatos de vehiculos son:");
+          
+           }
+    public String calcularAnio(){
+        double anioActual = 2021-aniofabricacion;
+        
+        if(anioActual >=2){
+            return "NO DEBE PRESENTARSE A LA REVISION";
+        }else{
+            return "SI DEBE PRESENTARSE A LA REVISION";
+        }
     }
     
+    public String datos(){
+        String informacion = "INFORMACION DEL VEHICULO\n"
+                +"Propietario:"+ propietario+"\n"
+                +"Vlor Comercal:"+ valorComercial +"\n"
+                +"Placa:"+ placa + "\n"
+                +"Matricual:"+ matriculado + "\n"
+                +"Anio Fabricacion:"+ aniofabricacion + "\n";
+                
+                return informacion;
+       
+                
+    }
 }
+
+    
+
--- Vehiculos2/src/main/java/com/mycompany/vehiculos2/Carro.java
+++ Vehiculos2/src/main/java/com/mycompany/vehiculos2/Carro.java
@@ -4,14 +4,34 @@
  * and open the template in the editor.
  */
 package com.mycompany.vehiculos2;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author lenovo
  */
 public class Carro {
-     public static void main(String[] args) {
-        // TODO code application logic here
+
+    public static void main(String[] args) {
+         Atributos objeto = new Atributos();
+        String propietario = JOptionPane.showInputDialog("Introduce el Propietario ");
+
+        String valor = JOptionPane.showInputDialog("Introduce valor comercial");
+        int valorcomercial = Integer.parseInt(valor);
+        String color = JOptionPane.showInputDialog("Introduce el color ");
+        String placa = JOptionPane.showInputDialog("Introduce la Placa ");
+        String matriculado = JOptionPane.showInputDialog("Introduce si esta matriculado ");
+        String anio = JOptionPane.showInputDialog("Introduce Año de Matriculacion ");
+        int aniofabricacion = Integer.parseInt(anio);
+
+        objeto.setPropietario(propietario);
+        objeto.setValorComercial(valorcomercial);
+        objeto.setColor(color);
+        objeto.setMatriculado(matriculado);
+        objeto.setAniofabricacion(aniofabricacion);
+
+        JOptionPane.showMessageDialog(null, objeto.datos() + "\n" + objeto.calcularAnio() + "\n");
+
     }
-    
+
 }
